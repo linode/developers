b0cfeb3e61dc202394be7071024bf3ada847f755
Trevor Parker <tparker@linode.com>
2014-06-11 10:52:14 -0400

    Magical fix

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 activate :directory_indexes
+activate :alias
 
 #page 'search', :layout => :search
 page '*', :layout => :article_layout
@@ -21,59 +22,7 @@ after_configuration do
   end
 end
 
-activate :alias
-
-###/
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# fixme: development shim
-# %x{
-#   rsync -a --exclude .git --delete ~/git/library-articles/articles/* ~/git/library-ng/source/library
-# }
+page "/sitemap.xml", :layout => false
 
 # Build-specific configuration
 configure :build do
@@ -92,6 +41,3 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
-
-page "/sitemap.xml", :layout => false
-
