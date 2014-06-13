32c98058143cf4ae3a11ea7157343380d24efc1f
Trevor Parker <tparker@linode.com>
2014-06-13 16:47:48 -0400

    First bit of JSON feed generator

--- config.rb
+++ config.rb
@@ -1,3 +1,72 @@
+class JSONFeed < Middleman::Extension
+  def initialize(app, options_hash={}, &block)
+    super
+
+    require 'json'
+  end
+
+  def after_build(builder)
+    @builder = builder
+    generate_feed
+  end
+
+  def generate_feed
+    feed = []
+    root = app.sitemap.resources.find_all { |p| p.destination_path.match(/^index\.html$/) }
+    tile_resources = root.first.data.tiles.map do |t|
+      path = t['url'].gsub!(/^\//, '')
+      app.sitemap.resources.find_all { |p| p.destination_path.match(/^#{path}\/index\.html/) }
+    end
+
+    tiles = tile_resources.map do |t|
+      type = (t.first.metadata[:options][:layout] == :categories_layout) ? 'category' : 'article'
+      tile = {
+        relative_path: "/#{t.first.destination_path.gsub(/\/index\.html$/, '')}",
+        title: t.first.data.title,
+        type: type,
+      }
+      if type == 'category' && t.first.children
+        tile['children'] = recurse_tree(t.first)
+      end
+      tile
+    end
+
+    result = {
+      last_modified: "1402593664",
+      sitemap: [
+        relative_path: nil,
+        title: 'Guides',
+        type: 'category',
+        children: tiles,
+      ]
+    }
+    File.open('build/sitemap.json', 'w') { |f| f.write(result.to_json) }
+
+    @builder.say_status :create, 'build/sitemap.json'
+    return 'build/sitemap.json'
+  end
+
+  def recurse_tree(parent, feed=[])
+    parent.children.each do |c|
+      next unless c.data.title
+      type = (c.metadata[:options][:layout] == :categories_layout) ? 'category' : 'article'
+      nugget = {
+        relative_path: "/#{c.destination_path.gsub(/\/index\.html$/, '')}",
+        title: c.data.title,
+        type: type,
+      }
+      if type == 'category' && c.children
+        nugget['children'] = recurse_tree(c)
+      end
+      feed << nugget
+    end
+
+    feed
+  end
+end
+
+::Middleman::Extensions.register(:json_feed, JSONFeed)
+
 activate :directory_indexes
 activate :alias
 
@@ -23,6 +92,8 @@ after_configuration do
 end
 
 page "/sitemap.xml", :layout => false
+
+activate :json_feed
 
 # Build-specific configuration
 configure :build do
