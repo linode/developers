ddf1f2a32f9992e237f337b8fe25e44d6f698ccd
Brian O'Keefe <brian@bokstuff.com>
2014-07-08 12:11:31 -0400

    Prefix assets manually

--- config.rb
+++ config.rb
@@ -139,7 +139,7 @@ configure :build do
   # activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "/library/"
+  # set :http_prefix, "/library/"
 end
 
 configure :development do
