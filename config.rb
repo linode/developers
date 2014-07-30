c5e3572cc13fae778be803ab1a147cc3601b869e
Brian O'Keefe <brianok@linode.com>
2014-07-30 16:35:46 -0400

    Revert "Pull category layout logic out into a separate file"
    This reverts commit 4c04f6adb0b600126a62585535570daa6467f5cb.

--- config.rb
+++ config.rb
@@ -2,12 +2,9 @@ activate :directory_indexes
 activate :alias
 
 require 'lib/extensions/library_alias'
-Dir['lib/pages/*.rb'].each { |f| require f }
 
 page '*', :layout => :article_layout
-page '*/index.html', :layout => :categories_layout do |app|
-  @scope = Docsmith::Pages.categories app.current_page
-end
+page '*/index.html', :layout => :categories_layout
 
 set :js_dir, 'js'
 set :font_dir, 'fonts'
