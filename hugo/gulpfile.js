e083665eba6d6f8e8b76ce4bde1e5e5293ac4dc5
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-09-20 11:36:39 +0200

    Format Gulp file

--- hugo/gulpfile.js
+++ hugo/gulpfile.js
@@ -15,40 +15,44 @@ gulp.task('default', ['watch']);
 gulp.task('server', ['watch']);
 
 
-gulp.task('build', [ 'js', 'css']);
+gulp.task('build', ['js', 'css']);
 gulp.task('build-all', function(cb) {
-  runSequence('clean-build', 'vendors', 'fonts',
-              'js-libs', 'build',
-              cb);
+    runSequence('clean-build', 'vendors', 'fonts',
+        'js-libs', 'build',
+        cb);
 });
 
 
 var vendors = ['bootstrap', 'font-awesome/less', 'font-awesome/fonts'];
 
-gulp.task('clean-vendors', function () {
-  return gulp.src('assets/vendors/', {read: false})
-    .pipe(clean());
+gulp.task('clean-vendors', function() {
+    return gulp.src('assets/vendors/', {
+            read: false
+        })
+        .pipe(clean());
 });
 
-gulp.task('clean-build', function () {
-  return gulp.src('static/build/', {read: false})
-    .pipe(clean());
+gulp.task('clean-build', function() {
+    return gulp.src('static/build/', {
+            read: false
+        })
+        .pipe(clean());
 });
 
 gulp.task('vendors', ['clean-vendors'], function() {
-  return merge(vendors.map(function(vendor) {
-    return gulp.src('node_modules/' + vendor + '/**/*')
-        .pipe(gulp.dest('assets/vendors/' + vendor));
-  }));
+    return merge(vendors.map(function(vendor) {
+        return gulp.src('node_modules/' + vendor + '/**/*')
+            .pipe(gulp.dest('assets/vendors/' + vendor));
+    }));
 });
 
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
     return gulp.src(['assets/vendors/bootstrap/dist/fonts/*', 'assets/vendors/font-awesome/fonts/*'])
         .pipe(gulp.dest('static/build/fonts'))
         .on('error', gutil.log);
 });
 
-gulp.task('js-libs', function () {
+gulp.task('js-libs', function() {
     return gulp.src(['assets/js/libs/**/*.js', 'assets/vendors/bootstrap/dist/js/bootstrap.js'])
         .pipe(order([
             "handlebars*.js",
@@ -60,40 +64,40 @@ gulp.task('js-libs', function () {
         .pipe(gulp.dest('static/build/js'))
         .pipe(plugins.uglify())
         .pipe(rename('libs.min.js')
-        .pipe(gulp.dest('static/build/js'))
-        .on('error', gutil.log))
+            .pipe(gulp.dest('static/build/js'))
+            .on('error', gutil.log))
 });
 
-gulp.task('js', function () {
+gulp.task('js', function() {
     return gulp.src('assets/js/*.js')
         .pipe(plugins.concat('main.js'))
         .pipe(gulp.dest('static/build/js'))
         .pipe(plugins.uglify())
         .pipe(rename('main.min.js')
-        .pipe(gulp.dest('static/build/js'))
-        .on('error', gutil.log))
+            .pipe(gulp.dest('static/build/js'))
+            .on('error', gutil.log))
 });
 
-gulp.task('css', function () {
+gulp.task('css', function() {
     return gulp.src('assets/stylesheets/home.less')
         .pipe(plugins.plumber())
         .pipe(plugins.less())
-        .on('error', function (err) {
+        .on('error', function(err) {
             gutil.log(err);
             this.emit('end');
         })
         .pipe(plugins.autoprefixer({
             browsers: [
-                    '> 1%',
-                    'last 2 versions',
-                    'firefox >= 4',
-                    'safari 7',
-                    'safari 8',
-                    'IE 8',
-                    'IE 9',
-                    'IE 10',
-                    'IE 11'
-                ],
+                '> 1%',
+                'last 2 versions',
+                'firefox >= 4',
+                'safari 7',
+                'safari 8',
+                'IE 8',
+                'IE 9',
+                'IE 10',
+                'IE 11'
+            ],
             cascade: false
         }))
         .pipe(plugins.cssmin())
@@ -124,10 +128,8 @@ gulp.task('hugo-server', function(cb) {
 });
 
 // Default task
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('assets/js/libs/**/*.js', ['js-libs']);
     gulp.watch('assets/js/*.js', ['js']);
     gulp.watch('assets/less/**/*.less', ['css']);
 });
-
-
