80eaedb7f3f826b249b73d79cbf536c389d24ea4
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-09-21 12:02:50 +0200

    Update to latest JQuery
    See #10

--- hugo/assets/js/main.js
+++ hugo/assets/js/main.js
@@ -158,7 +158,7 @@
                 author: bootply.com
              */
             /* smooth scrolling sections */
-            $('a[href*=#]:not([href=#])').click(function() {
+            $('a[href*=\\#]:not([href=\\#])').click(function() {
                 if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                     var target = $(this.hash);
                     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
