11de0e279fb62b7ff0aaf021d83831c38291a1dc
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-09-27 09:42:56 +0200

    Add deployment scripts

---
+++ tasks/deploy.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    argv = require('minimist')(process.argv),
+    rsync = require('gulp-rsync'),
+    prompt = require('gulp-prompt'),
+    gulpif = require('gulp-if');
+
+
+var rsyncConf = {
+
+}
+
+gulp.task('deploy:prepare', function() {
+
+    rsyncConf = {
+        progress: false,
+        incremental: true,
+        relative: true,
+        emptyDirectories: true,
+        recursive: true,
+        clean: true,
+        exclude: [],
+        rsyncPaths: ['public'],
+        pass: '',
+    };
+
+    if (argv.staging) {
+        rsyncConf.hostname = '50.116.61.198';
+        rsyncConf.username = 'bjorn';
+        rsyncConf.destination = '/home/bjorn';
+    } else if (argv.production) {
+        rsyncConf.hostname = '';
+        rsyncConf.username = '';
+        rsyncConf.destination = '';
+    } else {
+        throwError('deploy', gutil.colors.red('Missing or invalid target'));
+    }
+});
+
+
+gulp.task('deploy:remote', ['deploy:prepare'], function() {
+	 return gulp.src(rsyncConf.rsyncPaths)
+        .pipe(gulpif(
+            argv.production,
+            prompt.confirm({
+                message: 'Are you sure you want to deploy to production?',
+                default: false
+            })
+        ))
+        .pipe(rsync(rsyncConf));
+
+});
+
+
+function throwError(taskName, msg) {
+    throw new gutil.PluginError({
+        plugin: taskName,
+        message: msg
+    });
+}
