232c3a14164d1699383670ee353c77167f189301
Kayla Wilkins <kwilkins@linode.com>
2018-09-27 14:54:12 -0400

    attempting to set each set of vars as their own object to be exported

--- themes/dlc/tailwind.js
+++ themes/dlc/tailwind.js
@@ -1,19 +1,53 @@
 const config = require('../linode-hugo-theme/tailwind');
 
-config.colors['l-green'] = '#1AB15C'
-config.colors['l-nav-grey'] = '#a6a6a6'
-config.colors['l-full-black'] = '#000'
-config.colors['d-header-green'] = '#2F9072'
-config.colors['d-turq'] = '#346C8A'
-config.colors['d-turq-hov'] = '#4B81D5'
-config.colors['d-cell'] = '#EBEFF0'
-config.colors['d-blue-light'] = '#EDF7FE'
-config.colors['d-beige'] = '#ECECEC'
+const colors = {
+  'l-green': '#1AB15C',
+  'l-nav-grey': '#a6a6a6',
+  'l-full-black': '#000',
+  'l-text': '#333132',
+  'l-focus': '#107C9E',
+  'l-red': '#D41515',
+  'l-light-red': '#efdfde',
+  'l-grey': '#f4f4f4',
+  'l-off-white': '#fbfbfb',
+  'l-yellow': '#FFD006',
+  'd-turq': '#346C8A',
+  'd-turq-hov': '#4B81D5',
+}
 
-config.borderWidths['5'] = '5px'
+const borderWidths = {
+  '5': '5px',
+}
 
-config.padding['10'] = '2.5rem'
-config.padding['12'] = '3rem'
-config.padding['20'] = '5rem'
+const padding = {
+  '10': '2.5rem',
+  '12': '3rem',
+  '20': '5rem',
+}
 
+<<<<<<< HEAD
 module.exports = config;
+=======
+// config.colors['l-green'] = '#1AB15C'
+// config.colors['l-nav-grey'] = '#a6a6a6'
+// config.colors['l-full-black'] = '#000'
+// config.colors['d-header-green'] = '#2F9072'
+// config.colors['d-turq'] = '#346C8A'
+// config.colors['d-turq-hov'] = '#4B81D5'
+// config.colors['d-cell'] = '#EBEFF0'
+// config.colors['d-blue-light'] = '#EDF7FE'
+// config.colors['d-beige'] = '#ECECEC'
+
+// config.borderWidths['5'] = '5px'
+
+// config.padding['10'] = '2.5rem'
+// config.padding['12'] = '3rem'
+// config.padding['20'] = '5rem'
+
+module.exports = {
+  config,
+  colors,
+  borderWidths,
+  padding
+}
+>>>>>>> attempting to set each set of vars as their own object to be exported
