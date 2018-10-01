fc09d674ed2503833fa6ea2168838770ed21d2d7
Alban Bailly <abailly@linode.com>
2018-10-01 12:27:23 -0400

    simplify exports

--- themes/dlc/tailwind.js
+++ themes/dlc/tailwind.js
@@ -9,41 +9,4 @@ config.padding['10'] = '2.5rem'
 config.padding['12'] = '3rem'
 config.padding['20'] = '5rem'
 
-module.exports = {
-  colors: config.colors,
-  screens: config.screens,
-  fonts: config.fonts,
-  textSizes: config.textSizes,
-  fontWeights: config.fontWeights,
-  leading: config.leading,
-  tracking: config.tracking,
-  textColors: config.colors,
-  backgroundColors: config.colors,
-  backgroundSize: config.backgroundSize,
-  borderWidths: config.borderWidths,
-  borderColors: global.Object.assign({ default: config.colors['grey-light'] }, config.colors),
-  borderRadius: config.borderRadius,
-  width: config.width,
-  height: config.height,
-  minWidth: config.minWidth,
-  minHeight: config.minHeight,
-  maxWidth: config.maxWidth,
-  maxHeight: config.maxHeight,
-  padding: config.padding,
-  margin: config.margin,
-  negativeMargin: config.negativeMargin,
-  shadows: config.shadows,
-  zIndex: config.zIndex,
-  opacity: config.opacity,
-  svgFill: config.svgFill,
-  svgStroke: config.svgStroke,
-
-  plugins: [
-  ],
-
-  options: {
-    prefix: '',
-    important: false,
-    separator: ':',
-  },
-}
+module.exports = config;
