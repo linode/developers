deb2cdda25b332e5a500836e5b4e54cd40a845d2
Alban Bailly <abailly@linode.com>
2019-03-11 09:25:56 -0400

    cleanup

--- src/components/2_molecules/sidemenu.js
+++ src/components/2_molecules/sidemenu.js
@@ -2,29 +2,18 @@ import React from "react";
 import { graphql, StaticQuery } from "gatsby";
 
 const SideMenu = ({ data }) => {
-  const nodes = data.allPaths.edges;
   const groups = data.allPaths.group;
-
-  // console.log(data);
 
   return (
     <ul>
       {groups.map((group, i) => {
-        // const n = group.edges;
-
-        // console.log(
-        //   group.edges.map(link => {
-        //     return link.node.get && link.node.get.summary;
-        //   })
-        // );
-
         return (
           <div key={i}>
             <h2>{group.fieldValue}</h2>
-            {group.edges.map(link => {
+            {group.edges.map((link, i) => {
               const n = link.node;
               return (
-                <div>
+                <div key={i}>
                   {n.get && (
                     <li className="list-reset">
                       <a href={`/api/v4${n.name}#get`}>{n.get.summary}</a>
@@ -55,23 +44,6 @@ export default props => (
     query={graphql`
       query {
         allPaths {
-          edges {
-            node {
-              name
-              get {
-                summary
-                tags
-              }
-              post {
-                summary
-                tags
-              }
-              put {
-                summary
-                tags
-              }
-            }
-          }
           group(field: get___tags) {
             field
             fieldValue
@@ -96,30 +68,3 @@ export default props => (
     render={data => <SideMenu data={data} {...props} />}
   />
 );
-
-{
-  /* {nodes.map((node, i) => {
-        const n = node.node;
-
-        return (
-          <div key={i}>
-            <h1>{n.get && n.get.tags}</h1>
-            {n.get && (
-              <li className="list-reset">
-                <a href={`/api/v4${n.name}#get`}>{n.get.summary}</a>
-              </li>
-            )}
-            {n.post && (
-              <li className="list-reset">
-                <a href={`/api/v4${n.name}#post`}>{n.post.summary}</a>
-              </li>
-            )}
-            {n.put && (
-              <li className="list-reset">
-                <a href={`/api/v4${n.name}#put`}>{n.put.summary}</a>
-              </li>
-            )}
-          </div>
-        );
-      })} */
-}
