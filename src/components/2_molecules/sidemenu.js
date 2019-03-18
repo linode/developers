82243958e26444e016b42f85280fa26c568af69b
Alban Bailly <abailly@linode.com>
2019-03-18 11:05:24 -0400

    collapsible nav

--- src/components/2_molecules/sidemenu.js
+++ src/components/2_molecules/sidemenu.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { graphql, StaticQuery } from "gatsby";
 import { Link } from "gatsby";
-import Collapse, { Panel } from "rc-collapse";
+// import Collapse, { Panel } from "rc-collapse";
 import SearchHeader from "./search-header";
 
-import "rc-collapse/assets/index.css";
+// import "rc-collapse/assets/index.css";
 
 const _ = require("lodash");
 
@@ -12,83 +12,98 @@ class SideMenu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activePage: "",
-      activeKey: []
+      activePage: ""
     };
   }
 
-  onChange = activeKey => {
-    this.setState({
-      activeKey
-    });
-  };
+  componentDidMount() {
+    const url = document.location.pathname;
+    const urlSplit = url.split("/");
+    const location = urlSplit[3];
 
-  setActivePage = link => {
     this.setState({
-      activePage: link
+      activePage: location
     });
-  };
+    setTimeout(() => {
+      const activeEl = document.getElementsByClassName("active")[0];
+      const activeGroup = activeEl.parentNode.parentNode.parentNode.parentNode;
+      activeGroup.classList.add("active-group");
+    });
+  }
 
-  // componentDidMount() {}
+  toggleActive = e => {
+    const listGroup = e.target.closest("div");
+
+    console.log(listGroup);
+
+    if (listGroup && listGroup.classList.contains("active-group")) {
+      listGroup.classList.remove("active-group");
+    } else if (listGroup) {
+      listGroup.classList.add("active-group");
+    }
+  };
 
   render() {
-    const { activeKey, activePage } = this.state;
+    const { activePage } = this.state;
     const groups = this.props.data.allPaths.group;
-    // const location = window.location;
-    console.log(activePage);
+
+    // console.log(activeKey);
+
     return (
       <div>
         <SearchHeader />
-        <Collapse
-          accordion={true}
-          // activeKey={activeKey}
-          // onChange={this.onChange}
-        >
+        <div>
           {groups.map((group, i) => {
-            // console.log(activeKey);
             return (
-              <Panel header={group.fieldValue} key={i}>
-                {group.edges.map((link, i) => {
-                  const n = link.node;
-                  return (
-                    <ul key={i}>
-                      {n.get && (
-                        <li className="list-reset">
-                          <Link
-                            to={`/api/v4/${_.kebabCase(n.name)}`}
-                            onClick={() => this.setActivePage(n.name)}
-                          >
-                            {n.get.summary}
-                          </Link>
-                        </li>
-                      )}
-                      {n.post && (
-                        <li className="list-reset">
-                          <Link
-                            to={`/api/v4/${_.kebabCase(n.name)}#post`}
-                            onClick={() => this.setActivePage(n.name)}
-                          >
-                            {n.post.summary}
-                          </Link>
-                        </li>
-                      )}
-                      {n.put && (
-                        <li className="list-reset">
-                          <Link
-                            to={`/api/v4/${_.kebabCase(n.name)}#put`}
-                            onClick={() => this.setActivePage(n.name)}
-                          >
-                            {n.put.summary}
-                          </Link>
-                        </li>
-                      )}
-                    </ul>
-                  );
-                })}
-              </Panel>
+              <div className="list-container" key={i}>
+                <a
+                  href="javascript:;"
+                  onClick={this.toggleActive}
+                  className="block"
+                >
+                  <h3 className="mt-0">{group.fieldValue}</h3>
+                </a>
+                <div id={_.kebabCase(group.fieldValue)} className="list-group">
+                  {group.edges.map((link, i) => {
+                    const n = link.node;
+                    return (
+                      <ul key={i} className="p-0">
+                        {n.get && (
+                          <li className="list-reset">
+                            <Link
+                              to={`/api/v4/${_.kebabCase(n.name)}`}
+                              className={
+                                _.kebabCase(n.name) === activePage
+                                  ? "active"
+                                  : null
+                              }
+                            >
+                              {n.get.summary}
+                            </Link>
+                          </li>
+                        )}
+                        {n.post && (
+                          <li className="list-reset">
+                            <Link to={`/api/v4/${_.kebabCase(n.name)}#post`}>
+                              {n.post.summary}
+                            </Link>
+                          </li>
+                        )}
+                        {n.put && (
+                          <li className="list-reset">
+                            <Link to={`/api/v4/${_.kebabCase(n.name)}#put`}>
+                              {n.put.summary}
+                            </Link>
+                          </li>
+                        )}
+                      </ul>
+                    );
+                  })}
+                </div>
+              </div>
             );
           })}
-        </Collapse>
+        </div>
       </div>
     );
   }
