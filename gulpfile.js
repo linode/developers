f625a75b536ee34eac78502954e6770b67a4d87b
Jared Kobos <jkobos@linode.com>
2019-03-25 08:45:44 -0400

    Dynamic iframe rebuilt (#226)
    * Dynamic iframe url
    
    - Adds a config.toml for each of our environments (production, staging, testing, development)
    - api.html reads an environment variable from the current environment to set the docs iframe
    url.
    
    - To test, run hugo with the corresponding environment flag, e.g.:
    
    hugo server --environment staging
    
    * Rebuild dynamic iframe url PR

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 var runSequence = require('run-sequence');
 
-gulp.task('get-base-theme', 
+gulp.task('get-base-theme',
   shell.task(
     './dev_setup.sh',
   )
 );
 
-gulp.task('start-theme-tools', 
+gulp.task('start-theme-tools',
   shell.task(
     'cd themes/dlc && yarn && gulp watch'
   )
 );
 
-gulp.task('start-hugo', 
+gulp.task('start-hugo',
   shell.task(
     'hugo server -D'
   )
