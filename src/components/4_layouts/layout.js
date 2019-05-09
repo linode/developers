3df676e0562d60dfbfd4534cddbd5ccd215b82b5
Kayla Wilkins <kwilkins@linode.com>
2019-05-09 11:56:06 -0400

    addressing PR feedback

--- src/components/4_layouts/layout.js
+++ src/components/4_layouts/layout.js
@@ -36,20 +36,19 @@ const Layout = ({ children, title, subtitle, fullWidth, noFooter }) => (
         >
           <main className="main">{children}</main>
         </div>
-        {!noFooter && !fullWidth && (
-          <>
-            <div className="footer-nav-wrapper bg-black">
-              <footer className="max-w-3xl mx-auto py-8 px-4 md:px-8 text-white">
-                <Footer />
-              </footer>
-            </div>
-
-            <div className="footer-wrapper bg-BaseBlackFull">
-              <footer className="max-w-3xl mx-auto py-8 px-4 md:px-8 text-white">
-                <FooterNav />
-              </footer>
-            </div>
-          </>
+        {!fullWidth && (
+          <div className="footer-wrapper border-top">
+            <footer className="max-w-3xl mx-auto py-8 px-4 md:px-8 text-BaseBlackFull">
+              <FooterNav />
+            </footer>
+          </div>
+        )}
+        {!fullWidth && (
+          <div className="footer-nav-wrapper border-top">
+            <footer className="max-w-3xl mx-auto py-8 px-4 md:px-8 text-BaseBlackFull">
+              <Footer />
+            </footer>
+          </div>
         )}
       </div>
     )}
