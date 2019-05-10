c71eaf59c0d78f86e4357ff1f0d49df3b2437b72
Alban Bailly <abailly@linode.com>
2019-05-10 08:55:26 -0400

    add missing link

--- src/components/1_atoms/APITitle.js
+++ src/components/1_atoms/APITitle.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
 
 const APITitle = ({ data }) => {
   const n = data.allV4Json.edges[0].node;
 
-  return <div className="text-2xl mb-4">Linode API {n.info.version}</div>;
+  return (
+    <div className="mb-4">
+      <div className="mb-4">
+        <Link to="/">developers.linode.com</Link>
+      </div>
+      <div className="text-2xl">Linode API {n.info.version}</div>
+    </div>
+  );
 };
 
 export default props => (
