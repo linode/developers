61912fc7137bce5bf1339e46095ac40a6443429f
Kayla Wilkins <kwilkins@linode.com>
2019-06-12 15:25:04 -0400

    making changelog entries linkable with hash

--- src/components/2_molecules/changelog-item.js
+++ src/components/2_molecules/changelog-item.js
@@ -1,43 +1,68 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ChangelogItem = ({ title, date, version, changelog, html }) => (
-  <div
-    className="flex flex-wrap"
-    itemScope=""
-    itemType="http://schema.org/CreativeWork"
-  >
-    <meta itemProp="author" content="Linode" />
-    <div className="w-full md:w-1/4 py-2 md:p-6 md:text-right mt-1 flex items-center md:block">
+class ChangelogItem extends React.Component {
+  componentDidMount() {
+    const hash = document.location.hash;
+
+    hash &&
+      document.getElementById(hash.substr(1)) !== null &&
+      window.setTimeout(
+        () =>
+          document
+            .getElementById(hash.substr(1))
+            .scrollIntoView({ block: "start", behavior: "smooth" }),
+        50
+      );
+  }
+
+  render() {
+    const { title, date, version, changelog, html } = this.props;
+    const productTitle =
+      changelog.toString() === "Cloud Manager" ? "Cloud" : "API";
+    const link = `${productTitle}-${version}`;
+    return (
       <div
-        className="timeago mr-4 md:mr-0"
-        dateTime=""
-        itemProp="datePublished"
+        className="flex flex-wrap"
+        itemScope=""
+        itemType="http://schema.org/CreativeWork"
       >
-        {date}
-      </div>
-      <div className="text-BaseBlue md:mt-3 flex md:block ">
-        <span className="changelog-product" itemProp="name" />
-        {changelog && changelog}
-        {version && (
-          <div className="text-BaseBlue ml-4 md:ml-0" itemProp="version">
-            {version}
+        <meta itemProp="author" content="Linode" />
+        <div className="w-full md:w-1/4 py-2 md:p-6 md:text-right mt-1 flex items-center md:block">
+          <div
+            className="timeago mr-4 md:mr-0"
+            dateTime=""
+            itemProp="datePublished"
+          >
+            {date}
           </div>
-        )}
+          <div className="text-BaseBlue md:mt-3 flex md:block ">
+            <span className="changelog-product" itemProp="name" />
+            {changelog && changelog}
+            {version && (
+              <div className="text-BaseBlue ml-4 md:ml-0" itemProp="version">
+                {version}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="w-full md:w-3/4 py-2 md:p-6" id={link}>
+          <h2
+            className="mt-0 text-2xl font-normal"
+            itemProp="alternativeHeadline"
+          >
+            {title}
+          </h2>
+          <div
+            className="changelog-entry"
+            itemProp="disambiguatingDescription"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
-    <div className="w-full md:w-3/4 py-2 md:p-6">
-      <h2 className="mt-0 text-2xl font-normal" itemProp="alternativeHeadline">
-        {title}
-      </h2>
-      <div
-        className="changelog-entry"
-        itemProp="disambiguatingDescription"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 ChangelogItem.propTypes = {
   title: PropTypes.string.isRequired
