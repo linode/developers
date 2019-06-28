83eb342becf9fac8c96332239a88e54d216ddce4
Alban Bailly <abailly@linode.com>
2019-06-27 22:43:52 -0400

    fine tuning the samples

--- src/components/2_molecules/ResponseSampleBody.js
+++ src/components/2_molecules/ResponseSampleBody.js
@@ -4,6 +4,86 @@ import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export const ResponseSampleBody = props => {
   const { context, response } = props;
+
+  const sampleSource = `{
+      ${context.content &&
+        context.content.application_json &&
+        context.content.application_json.schema &&
+        context.content.application_json.schema.properties &&
+        Object.keys(context.content.application_json.schema.properties)
+          .filter(
+            v => context.content.application_json.schema.properties[v] !== null
+          )
+          .map((p, i) => {
+            const l = context.content.application_json.schema.properties[p];
+
+            return (
+              l &&
+              (l.type !== "array" && p !== "errors"
+                ? `"${p}": ${
+                    l.example
+                      ? l.type !== "number" &&
+                        l.type !== "integer" &&
+                        l.type !== "boolean"
+                        ? `${JSON.stringify(l.example)}`
+                        : `${l.example}`
+                      : p === "id"
+                      ? `${'"2737bf16b39ab5d7b4a1"'}`
+                      : l.type === "boolean"
+                      ? `${"false"}`
+                      : !l.properties
+                      ? `${'"0"'}`
+                      : ""
+                  }`
+                : `"${p}": [
+                  ${l.example &&
+                    l.example.map((e, i) => {
+                      return `"${e}"`;
+                    })}
+                  ${!(l.items && l.items.properties) ? `${"]"}` : ""}
+              `) +
+                (l.properties &&
+                  `{
+                  ${Object.keys(l.properties).map((e, i) => {
+                    const data = l.properties[e];
+                    return `"${e}": ${
+                      data.type !== "number"
+                        ? `"${data.example}"`
+                        : `${data.example}`
+                    }`;
+                  })}
+                }
+              `) +
+                (l.items &&
+                  l.items.properties &&
+                  `
+                    {
+                    ${Object.keys(l.items.properties).map((e, i) => {
+                      const data = l.items.properties[e];
+                      return (
+                        data &&
+                        `"${e}": ${
+                          data.type !== "number"
+                            ? `"${data.example}"`
+                            : `${data.example}`
+                        }`
+                      );
+                    })}
+                  }
+                ]
+                `)
+            );
+          })}
+      }`;
+
+  const sanitized = sampleSource
+    .replace(/"undefined"/g, "")
+    .replace(/undefined/g, "")
+    .replace(/null/g, "");
+
+  const finalSource = JSON.stringify(JSON.parse(sanitized), null, 2);
+  //const finalSource = sanitized;
+
   return (
     context &&
     context[response] !== "" && (
@@ -15,114 +95,7 @@ export const ResponseSampleBody = props => {
           style: { whiteSpace: "pre-wrap" }
         }}
       >
-        <div>
-          {"{"}
-          {context.content &&
-            context.content.application_json &&
-            context.content.application_json.schema &&
-            context.content.application_json.schema.properties &&
-            Object.keys(context.content.application_json.schema.properties).map(
-              (p, i) => {
-                const l = context.content.application_json.schema.properties[p];
-                const rowLen = Object.keys(
-                  context.content.application_json.schema.properties
-                ).length;
-                return (
-                  l && (
-                    <div key={i} className="ml-4">
-                      <div>
-                        <div>
-                          {l.type !== "array" && p !== "errors" ? (
-                            <>
-                              "
-                              <span className={l.deprecated && "line-through"}>
-                                {p}
-                              </span>
-                              ":{" "}
-                              <span style={{ color: "#3BB878" }}>
-                                {/* "{l.example}"{" "} */}
-                                {l.properties ? "{" : `"${l.example}"`},
-                              </span>
-                            </>
-                          ) : (
-                            <>
-                              "{p}"{": ["}
-                              {l.example &&
-                                l.example.map((e, i) => {
-                                  const rowLen4 = l.example.length;
-                                  return (
-                                    <span style={{ color: "#3BB878" }} key={i}>
-                                      "{e}"{rowLen4 !== i + 1 && ","}
-                                    </span>
-                                  );
-                                })}
-                              {"]"}
-                            </>
-                          )}
-                        </div>
-                      </div>
-                      {l.properties && (
-                        <>
-                          {Object.keys(l.properties).map((e, i) => {
-                            const data = l.properties[e];
-                            const rowLen2 = Object.keys(l.properties).length;
-                            return (
-                              <div key={i} className="ml-4">
-                                <div>
-                                  {e}:{" "}
-                                  <span style={{ color: "#3BB878" }}>
-                                    "{data.example}"{rowLen2 !== i + 1 && ","}
-                                  </span>
-                                </div>
-                              </div>
-                            );
-                          })}
-                          {"  }"}
-                        </>
-                      )}
-                      {l.items &&
-                        (l.items.properties && (
-                          <div>
-                            {"  {"}
-                            {Object.keys(l.items.properties).map((e, i) => {
-                              const data = l.items.properties[e];
-                              const rowLen3 = Object.keys(l.items).length;
-                              return (
-                                data && (
-                                  <div key={i} className="ml-8">
-                                    <div>
-                                      "{e}":{" "}
-                                      <span style={{ color: "#3BB878" }}>
-                                        "{data.example}"
-                                        {rowLen3 === i + 1 && ","}
-                                      </span>
-                                    </div>
-                                  </div>
-                                )
-                              );
-                            })}
-                            {"  }"}
-                            {l.type !== "array" && (
-                              <div>
-                                {"]"}
-                                {rowLen !== i + 1 && ","}
-                              </div>
-                            )}
-                            {p !== "errors" && (
-                              <div>
-                                {"]"}
-                                {rowLen !== i + 1 && ","}
-                              </div>
-                            )}
-                          </div>
-                        ))}
-                    </div>
-                  )
-                );
-              }
-            )}
-          {"}"}
-        </div>
+        {finalSource}
       </SyntaxHighlighter>
     )
   );
