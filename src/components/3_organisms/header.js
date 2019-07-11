8962a10b5f0ded022c257dbf661bf23fe05f62a2
Alban Bailly <abailly@linode.com>
2019-07-11 14:51:11 -0400

    styling sub menus

--- src/components/3_organisms/header.js
+++ src/components/3_organisms/header.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { Link } from "gatsby";
-import MainSiteGlobalMenu from "../2_molecules/MainSiteGlobalMenu";
-import MainSiteNav from "../2_molecules/MainSiteNav";
+import UtilityNav from "../2_molecules/navigation/utilityNav";
+import PrimaryNav from "../2_molecules/navigation/primaryNav";
 
 import Logo from "../../images/svgs/new-logo.svg";
 
-const Header = ({ border }) => (
-  <div
-    className={`header-wrapper bg-white ${border ? "border-b border-api" : ""}`}
-  >
+const Header = () => (
+  <div className="header__wrapper">
     <header className="header">
-      <div className="border-b border-api">
-        <MainSiteGlobalMenu />
+      <div className="utility-nav__wrapper">
+        <UtilityNav />
       </div>
-      <div className="primaryLinks-container max-w-3xl mx-auto">
-        <div className="primaryLinks-inner flex flex-wrap items-center justify-between">
+      <div className="primary-nav__wrapper">
+        <div className="primary-nav__inner">
           <div className="logo">
             <Link to="/" className="flex items-center">
               <Logo />
             </Link>
           </div>
           <div>
-            <MainSiteNav />
+            <PrimaryNav />
           </div>
         </div>
       </div>
