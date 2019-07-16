e2fc5fb4ee639fd5122fb84371a5e1e05144379f
Alban Bailly <abailly@linode.com>
2019-07-16 08:25:18 -0400

    better accessibility

--- src/components/2_molecules/navigation/primaryNav.js
+++ src/components/2_molecules/navigation/primaryNav.js
@@ -52,7 +52,7 @@ class MainSiteNav extends React.Component {
 
     document.onkeydown = function(evt) {
       evt = evt || window.event;
-      if (evt.keyCode == 27) {
+      if (evt.keyCode === 27) {
         hideAll();
       }
     };
