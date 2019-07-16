ad5b3b2735d6c21a4ef02114a0efeff6753e080e
Alban Bailly <abailly@linode.com>
2019-07-16 07:44:21 -0400

    wrap products menu and stles adjustments

--- src/components/2_molecules/navigation/productsMenus.js
+++ src/components/2_molecules/navigation/productsMenus.js
@@ -4,19 +4,27 @@ import ProductFeatured from "./productsFeatured";
 import ProductsCompute from "./productsCompute";
 import ProductsStorage from "./productsStorage";
 import ProductsServices from "./productsServices";
+import ProductsNetworking from "./productsNetworking";
+import ProductsDevTools from "./productsDevTools";
 
 const ProductMenus = () => (
   <>
-    <div class="sub-menu__col">
+    <div className="sub-menu__col">
       <div className="sub-menu__content">
         <ProductFeatured />
       </div>
     </div>
-    <div class="sub-menu__col">
+    <div className="sub-menu__col">
       <div className="sub-menu__content">
         <ProductsCompute />
         <ProductsStorage />
         <ProductsServices />
+      </div>
+    </div>
+    <div className="sub-menu__col">
+      <div className="sub-menu__content">
+        <ProductsNetworking />
+        <ProductsDevTools />
       </div>
     </div>
   </>
