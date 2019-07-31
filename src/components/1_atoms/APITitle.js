528cc8e1eaf7a5cc20325f34c63b05dda754e61e
Alban Bailly <abailly@linode.com>
2019-07-31 13:45:35 -0400

    post rebase fixes

--- src/components/1_atoms/APITitle.js
+++ src/components/1_atoms/APITitle.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticQuery, graphql, Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 
 const APITitle = ({ data }) => {
   const n = data.allDataJson.edges[0].node;
