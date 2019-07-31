25cc58a7129b2830423e3296ab88c63b3732f1d2
Alban Bailly <abailly@linode.com>
2019-07-31 09:32:38 -0400

    fix build fail part 2

--- src/components/5_templates/api.js
+++ src/components/5_templates/api.js
@@ -28,190 +28,196 @@ const scrollToTop = () => {
   });
 };
 
-const apiPage = ({ data }) => {
-  const n = data.allPaths.edges[0].node;
-  const modes = {
-    get: "get",
-    put: "put",
-    post: "post",
-    delete: "delete"
-  };
-  const responseOptions = {
-    _200: "_200",
-    _204: "_204",
-    default: "default"
-  };
+class apiPage extends React.Component {
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const top = window.scrollY;
+      const scrollButton = document.getElementById("back-to-top");
+      if (top >= 50) {
+        scrollButton.classList.add("is-visible");
+      } else {
+        scrollButton.classList.remove("is-visible");
+      }
+    });
+  }
 
-  window.addEventListener("scroll", () => {
-    const top = window.scrollY;
-    const scrollButton = document.getElementById("back-to-top");
-    if (top >= 50) {
-      scrollButton.classList.add("is-visible");
-    } else {
-      scrollButton.classList.remove("is-visible");
-    }
-  });
+  render() {
+    const n = this.props.data.allPaths.edges[0].node;
+    const modes = {
+      get: "get",
+      put: "put",
+      post: "post",
+      delete: "delete"
+    };
+    const responseOptions = {
+      _200: "_200",
+      _204: "_204",
+      default: "default"
+    };
 
-  return (
-    <Layout fullWidth>
-      <SEO
-        title={
-          "Linode API | " + (n.get && n.get.tags) ||
-          (n.post && n.post.tags) ||
-          (n.put && n.put.tags) ||
-          (n.delete && n.delete.tags)
-        }
-        description={
-          (n.get && n.get.description ? n.get.description : "") +
-          (n.post && n.post.description ? n.post.description : "") +
-          (n.put && n.put.description ? n.put.description : "") +
-          (n.delete && n.delete.description ? n.delete.description : "")
-        }
-      />
-      <div className="flex flex-wrap">
-        <div className="md:hidden search-header-wrapper">
-          <SearchHeader />
-        </div>
-        <div className="sidebar-container">
-          <Sidebar />
-        </div>
-        <div className="w-full px-4 api-content-wrapper">
-          <div className="api-content mx-auto">
-            <h1 className="mb-4 ">
-              {(n.get && n.get.tags) ||
-                (n.post && n.post.tags) ||
-                (n.put && n.put.tags) ||
-                (n.delete && n.delete.tags)}
-            </h1>
-            {Object.keys(n).map((e, i) => {
-              const mode = modes[e];
-              const m = n[mode];
+    return (
+      <Layout fullWidth>
+        <SEO
+          title={
+            "Linode API | " + (n.get && n.get.tags) ||
+            (n.post && n.post.tags) ||
+            (n.put && n.put.tags) ||
+            (n.delete && n.delete.tags)
+          }
+          description={
+            (n.get && n.get.description ? n.get.description : "") +
+            (n.post && n.post.description ? n.post.description : "") +
+            (n.put && n.put.description ? n.put.description : "") +
+            (n.delete && n.delete.description ? n.delete.description : "")
+          }
+        />
+        <div className="flex flex-wrap">
+          <div className="md:hidden search-header-wrapper">
+            <SearchHeader />
+          </div>
+          <div className="sidebar-container">
+            <Sidebar />
+          </div>
+          <div className="w-full px-4 api-content-wrapper">
+            <div className="api-content mx-auto">
+              <h1 className="mb-4 ">
+                {(n.get && n.get.tags) ||
+                  (n.post && n.post.tags) ||
+                  (n.put && n.put.tags) ||
+                  (n.delete && n.delete.tags)}
+              </h1>
+              {Object.keys(n).map((e, i) => {
+                const mode = modes[e];
+                const m = n[mode];
 
-              return (
-                m && (
-                  <div key={i} className="flex flex-col">
-                    <span id={mode} className="endpoint-anchor" />
-                    <div className="xs-full mb-8">
-                      <h2 className="mt-0">{m.summary}</h2>
-                      <div className="bg-ThemeCell p-4 mt-4 mb-8 flex items-center justify-between flex-wrap">
-                        <div className="flex items-center mr-4">
-                          <span className="tag big bold mr-2 uppercase">
-                            {mode}
-                          </span>
-                          <pre className="whitespace-pre-line">
-                            {m.servers
-                              ? m.servers[0].url
-                              : "https://api.linode.com/v4"}
-                            {n.name}
-                          </pre>
+                return (
+                  m && (
+                    <div key={i} className="flex flex-col">
+                      <span id={mode} className="endpoint-anchor" />
+                      <div className="xs-full mb-8">
+                        <h2 className="mt-0">{m.summary}</h2>
+                        <div className="bg-ThemeCell p-4 mt-4 mb-8 flex items-center justify-between flex-wrap">
+                          <div className="flex items-center mr-4">
+                            <span className="tag big bold mr-2 uppercase">
+                              {mode}
+                            </span>
+                            <pre className="whitespace-pre-line">
+                              {m.servers
+                                ? m.servers[0].url
+                                : "https://api.linode.com/v4"}
+                              {n.name}
+                            </pre>
+                          </div>
+                          {m.servers &&
+                            m.servers[0].url ===
+                              "https://api.linode.com/v4beta" && (
+                              <span className="tag tag-beta">BETA</span>
+                            )}
                         </div>
-                        {m.servers &&
-                          m.servers[0].url ===
-                            "https://api.linode.com/v4beta" && (
-                            <span className="tag tag-beta">BETA</span>
-                          )}
-                      </div>
-                      <Markdown
-                        source={m.description}
-                        escapeHtml={false}
-                        className="my-8 api-desc"
-                      />
-                      {m.security && <Security oauth={m.security[1].oauth} />}
-                      {m.parameters && (
-                        <div className="my-8">
-                          <h3 className="mb-2">Query Parameters</h3>
-                          {m.parameters.map((param, i) => (
-                            <ParamDisplay
-                              key={`param-item-${i}`}
-                              param={param}
-                              m={m}
-                            />
-                          ))}
-                        </div>
-                      )}
-                      {n.parameters && (
-                        <div className="my-8">
-                          <h3 className="mb-2">Path Parameters</h3>
-                          {n.parameters.map((param, i) => (
-                            <ParamDisplay
-                              key={`param-item-${i}`}
-                              param={param}
-                              m={n}
-                            />
-                          ))}
-                        </div>
-                      )}
-                      {m.requestBody && <BodySchema data={m} />}
-                      <div className="w-full mb-8">
-                        <h3>Request Samples</h3>
-                        <Tabs className="my-4">
-                          <TabList>
+                        <Markdown
+                          source={m.description}
+                          escapeHtml={false}
+                          className="my-8 api-desc"
+                        />
+                        {m.security && <Security oauth={m.security[1].oauth} />}
+                        {m.parameters && (
+                          <div className="my-8">
+                            <h3 className="mb-2">Query Parameters</h3>
+                            {m.parameters.map((param, i) => (
+                              <ParamDisplay
+                                key={`param-item-${i}`}
+                                param={param}
+                                m={m}
+                              />
+                            ))}
+                          </div>
+                        )}
+                        {n.parameters && (
+                          <div className="my-8">
+                            <h3 className="mb-2">Path Parameters</h3>
+                            {n.parameters.map((param, i) => (
+                              <ParamDisplay
+                                key={`param-item-${i}`}
+                                param={param}
+                                m={n}
+                              />
+                            ))}
+                          </div>
+                        )}
+                        {m.requestBody && <BodySchema data={m} />}
+                        <div className="w-full mb-8">
+                          <h3>Request Samples</h3>
+                          <Tabs className="my-4">
+                            <TabList>
+                              {m.x_code_samples &&
+                                m.x_code_samples.map((x, i) => {
+                                  return <Tab key={i}>{x.lang}</Tab>;
+                                })}
+                            </TabList>
                             {m.x_code_samples &&
                               m.x_code_samples.map((x, i) => {
-                                return <Tab key={i}>{x.lang}</Tab>;
-                              })}
-                          </TabList>
-                          {m.x_code_samples &&
-                            m.x_code_samples.map((x, i) => {
-                              return (
-                                <TabPanel key={i}>
-                                  <div className="flex justify-end text-sm">
-                                    <Clipboard
-                                      data-clipboard-text={x.source}
-                                      className="flex items-center hover:text-BaseBlueLight"
+                                return (
+                                  <TabPanel key={i}>
+                                    <div className="flex justify-end text-sm">
+                                      <Clipboard
+                                        data-clipboard-text={x.source}
+                                        className="flex items-center hover:text-BaseBlueLight"
+                                      >
+                                        <span className="mr-2">Copy</span>
+                                        <Copy
+                                          style={{ width: 22, height: 22 }}
+                                        />
+                                      </Clipboard>
+                                    </div>
+                                    <SyntaxHighlighter
+                                      language="bash"
+                                      style={atomDark}
+                                      className="api-samples"
+                                      codeTagProps={{
+                                        style: { whiteSpace: "pre-wrap" }
+                                      }}
                                     >
-                                      <span className="mr-2">Copy</span>
-                                      <Copy style={{ width: 22, height: 22 }} />
-                                    </Clipboard>
-                                  </div>
-                                  <SyntaxHighlighter
-                                    language="bash"
-                                    style={atomDark}
-                                    className="api-samples"
-                                    codeTagProps={{
-                                      style: { whiteSpace: "pre-wrap" }
-                                    }}
-                                  >
-                                    {x.source}
-                                  </SyntaxHighlighter>
-                                </TabPanel>
-                              );
-                            })}
-                        </Tabs>
-                        <ResponseSamples
+                                      {x.source}
+                                    </SyntaxHighlighter>
+                                  </TabPanel>
+                                );
+                              })}
+                          </Tabs>
+                          <ResponseSamples
+                            options={responseOptions}
+                            responses={m.responses}
+                            m={m}
+                            mode={mode}
+                          />
+                        </div>
+
+                        <ResponseList
                           options={responseOptions}
                           responses={m.responses}
                           m={m}
                           mode={mode}
                         />
                       </div>
-
-                      <ResponseList
-                        options={responseOptions}
-                        responses={m.responses}
-                        m={m}
-                        mode={mode}
-                      />
                     </div>
-                  </div>
-                )
-              );
-            })}
+                  )
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className="back-to-top md:hidden"
-        onClick={scrollToTop}
-        id="back-to-top"
-      >
-        <span className="back-to-top__caret">
-          <Caret />
-        </span>
-      </div>
-    </Layout>
-  );
-};
+        <div
+          className="back-to-top md:hidden"
+          onClick={scrollToTop}
+          id="back-to-top"
+        >
+          <span className="back-to-top__caret">
+            <Caret />
+          </span>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default apiPage;
 
