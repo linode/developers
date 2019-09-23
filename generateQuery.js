0e1255b839bec7304fd5ea5953a9f9d51be99996
Alban Bailly <abailly@linode.com>
2019-09-23 12:47:37 -0400

    a bit more cleanup and fix undefined var

--- generateQuery.js
+++ generateQuery.js
@@ -1,33 +1,30 @@
-const printName = (name) => {
-  return ' ' + name;
-}
+const printName = name => {
+  return " " + name;
+};
 
 const handleNested = (fieldsPath, fieldType) => {
   const fields = recursiveQuery(fieldsPath);
-  return fields
-    ? ` ${fieldType} { ${fields} }`
-    : '';
-}
+  return fields ? ` ${fieldType} { ${fields} }` : "";
+};
 
 const _query = property => {
   // Special cases (nested values)
-  if (property.name === 'allOf') {
+  if (property.name === "allOf") {
     if (property.type.ofType) {
-      return handleNested(property.type.ofType.fields, 'allOf');
+      return handleNested(property.type.ofType.fields, "allOf");
     }
 
-    return handleNested(property.type.fields, 'allOf');
+    return handleNested(property.type.fields, "allOf");
   }
 
-  if (property.name === 'oneOf') {
+  if (property.name === "oneOf") {
     if (property.type.ofType) {
-      return handleNested(property.type.ofType.fields, 'oneOf');
+      return handleNested(property.type.ofType.fields, "oneOf");
     }
-    return handleNested(property.type.fields, 'oneOf');
+    return handleNested(property.type.fields, "oneOf");
   }
 
   // Base cases
-
   if (!property.type) {
     return printName(property.name);
   }
@@ -38,22 +35,27 @@ const _query = property => {
   }
 
   // Normal recursion
-
   if (property.type && property.type.fields) {
-    return `${printName(property.name)} { ${recursiveQuery(property.type.fields)} }`;
+    return `${printName(property.name)} { ${recursiveQuery(
+      property.type.fields
+    )} }`;
   }
 
   if (property.type && property.type.typeOf && property.type.typeOf.fields) {
-    return `${printName(property.name)} { ${recursiveQuery(property.type.typeOf.fields)} }`;
+    return `${printName(property.name)} { ${recursiveQuery(
+      property.type.typeOf.fields
+    )} }`;
   }
 
   // If we reach this point, there's a problem.
-  throw new Error('No cases matched.');
-}
+  throw new Error("No cases matched.");
+};
 
 const recursiveQuery = properties => {
-  if (properties === null) { return; }
+  if (properties === null) {
+    return;
+  }
   return properties.map(property => _query(property));
-}
+};
 
-module.exports = { rawQuery, recursiveQuery };
+module.exports = { recursiveQuery };
