7f7ad812d9cd8ab9d4ab1c952ff52b590542665d
bbiggerr <bxbigg@gmail.com>
2021-10-29 13:30:27 -0400

    Why Linode featured update

--- src/components/2_molecules/navigation/whyFeatured.js
+++ src/components/2_molecules/navigation/whyFeatured.js
@@ -11,36 +11,36 @@ const WhyFeaturedNav = () => (
       <a
         id="c-featured--why_linode"
         className="c-featured"
-        href="https://www.linode.com/spotlight/data-machines-brian-dennis/"
+        href="https://www.linode.com/content/macrometa/"
         style={styles}
         onClick={()=>
-          Event('Navigation','click','datamachines')
+          Event('Navigation','click','macrometa')
         }
       >
         <img
-          width="565"
-          height="249"
-          src="https://www.linode.com/wp-content/uploads/2020/12/brian-data-machines-background-MIN.jpg"
+          width="632"
+          height="279"
+          src="https://www.linode.com/wp-content/uploads/2021/04/macrometa5-opt-632x279.jpg"
           className="c-featured__background"
-          alt=""
+          alt="Durga Gokina, CTO and Head of R&amp;D, and Chetan Venkatesh, Co-Founder and CEO of Macrometa"
           loading="lazy"
-          srcSet="https://www.linode.com/wp-content/uploads/2020/12/brian-data-machines-background-MIN.jpg 565w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
+          srcset="https://www.linode.com/wp-content/uploads/2021/04/macrometa5-opt-632x279.jpg 632w, https://www.linode.com/wp-content/uploads/2021/04/macrometa5-opt-1064x470.jpg 1064w, https://www.linode.com/wp-content/uploads/2021/04/macrometa5-opt-1944x858.jpg 1944w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
           sizes="(max-width:991px) 1px, 565px"
         />
         <div className="c-featured__text">
           <div className="c-featured__headline">
             <b>Craft of Code:</b>
             <br/>
-            Dr. Brian Dennis
+            Macrometa
           </div>
           <div className="c-featured__excerpt">
-            How Data Machines Does Big Data
+            Bringing Innovation to the Edge
           </div>
           <span
             className="c-featured__button"
-            href="https://www.linode.com/spotlight/data-machines-brian-dennis/"
+            href="https://www.linode.com/content/macrometa/"
           >
-              Read Dr. Dennis' Story
+              Read Macrometa's Story
           </span>
         </div>
       </a>
