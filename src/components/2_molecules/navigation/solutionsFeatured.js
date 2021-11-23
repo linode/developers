6e15bfbfd8b59b2ed9ccb8bb32313785bf64f907
bbiggerr <bxbigg@gmail.com>
2021-11-23 16:18:39 -0500

    Update header featured to srcSet

--- src/components/2_molecules/navigation/solutionsFeatured.js
+++ src/components/2_molecules/navigation/solutionsFeatured.js
@@ -26,7 +26,7 @@ const SolutionsFeaturedNav = () => (
             className="c-featured__background"
             alt="The University of Oklahoma"
             loading="lazy"
-            srcset="https://www.linode.com/wp-content/uploads/2021/06/UO-632x279.jpg 632w, https://www.linode.com/wp-content/uploads/2021/06/UO-1064x470.jpg 1064w, https://www.linode.com/wp-content/uploads/2021/06/UO-1944x858.jpg 1944w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
+            srcSet="https://www.linode.com/wp-content/uploads/2021/06/UO-632x279.jpg 632w, https://www.linode.com/wp-content/uploads/2021/06/UO-1064x470.jpg 1064w, https://www.linode.com/wp-content/uploads/2021/06/UO-1944x858.jpg 1944w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
             sizes="(max-width:991px) 1px, 632px"
           />
           <div className="c-featured__text">
