31fcf0146916942988df1fa0db9319d500bd3bf6
bbiggerr <bxbigg@gmail.com>
2022-01-18 12:46:14 -0500

    Add H1 tags to banner titles

--- src/components/3_organisms/banner.js
+++ src/components/3_organisms/banner.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Banner = ({ title, subtitle }) => (
   <div className="banner__wrapper triangle-bg relative z-0">
     <header className="banner max-w-3xl mx-auto px-4 py-8 text-white">
-      <div className="text-4xl leading-none font-semibold">{title}</div>
+      <div className="text-4xl leading-none font-semibold"><h1>{title}</h1></div>
       <div className="text-3xl leading-none font-light mt-2">{subtitle}</div>
     </header>
   </div>
