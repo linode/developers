1c56388e4d5b08aa502212224e3f4531b7c91a27
Jason Tremblay <4190+alertmybanjos@users.noreply.github.com>
2022-03-11 14:12:36 -0500

    Update solutions submenu featured content

--- src/components/2_molecules/navigation/solutionsFeatured.js
+++ src/components/2_molecules/navigation/solutionsFeatured.js
@@ -13,7 +13,7 @@ const SolutionsFeaturedNav = () => (
         <a
           id="c-featured--solutions"
           className="c-featured"
-          href="https://www.linode.com/spotlight/the-university-of-oklahoma/"
+          href="https://www.linode.com/content/dev-mukherjee/"
           style={styles}
           onClick={()=>
             Event('Navigation','click','ou')
@@ -22,25 +22,22 @@ const SolutionsFeaturedNav = () => (
           <img
             width="632"
             height="279"
-            src="https://www.linode.com/wp-content/uploads/2021/06/UO-632x279.jpg"
+            src="https://www.linode.com/wp-content/uploads/2021/10/anomaly-ltd-header-632x279.jpg"
             className="c-featured__background"
-            alt="The University of Oklahoma"
+            alt="Dev Mukherjee"
             loading="lazy"
-            srcSet="https://www.linode.com/wp-content/uploads/2021/06/UO-632x279.jpg 632w, https://www.linode.com/wp-content/uploads/2021/06/UO-1064x470.jpg 1064w, https://www.linode.com/wp-content/uploads/2021/06/UO-1944x858.jpg 1944w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
+            srcSet="https://www.linode.com/wp-content/uploads/2021/10/anomaly-ltd-header-632x279.jpg 632w, https://www.linode.com/wp-content/uploads/2021/10/anomaly-ltd-header-1064x470.jpg 1064w, https://www.linode.com/wp-content/uploads/2021/10/anomaly-ltd-header-1944x858.jpg 1944w, data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7 1w"
             sizes="(max-width:991px) 1px, 632px"
           />
           <div className="c-featured__text">
             <div className="c-featured__headline">
-              <b>Higher Ed: The University of Oklahoma</b>
+              Dev Mukherjee <br/>@ Anomaly
             </div>
             <div className="c-featured__excerpt">
-              Developing Innovative Approaches <br/> to Understanding Social and Natural Risks
+              Innovation Down Under and Beyond:<br/> From Virtualization to Electric Vehicles
             </div>
-            <span
-              className="c-featured__button"
-              href="https://www.linode.com/spotlight/the-university-of-oklahoma/"
-            >
-              Read OU Story
+            <span className="c-featured__button">
+              Read Dev's Story
             </span>
           </div>
         </a>
