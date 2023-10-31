269e711cfd180e19b635c5fa91387eb75ecba2bf
Jason Tremblay <4190+alertmybanjos@users.noreply.github.com>
2023-10-31 14:17:16 -0400

    Header and footer changes:  - remove links to affiliates program  - CSS: fix banner link underline styles in Safari  - CSS: fix space reserved for Wegot dropdown (n/a to developers)

--- src/components/3_organisms/header.js
+++ src/components/3_organisms/header.js
@@ -464,9 +464,6 @@ class Header extends React.Component {
                   <a className="o-menu__link" href="https://www.linode.com/developers/"><span className="o-menu__title">Developer Portal</span></a>
                 </li>
                 <li className="o-menu__item">
-                  <a className="o-menu__link" href="https://www.linode.com/lp/affiliate-program/"><span className="o-menu__title">Affiliate Program</span></a>
-                </li>
-                <li className="o-menu__item">
                   <a className="o-menu__link" href="https://www.linode.com/green-light/"><span className="o-menu__title">Beta Program</span></a>
                 </li>
                 <li className="o-menu__item">
@@ -958,9 +955,6 @@ class Header extends React.Component {
                 </li>
                 <li className="o-menu__item">
                   <a className="o-menu__link" href="https://www.linode.com/developers/"><span className="o-menu__title">Developer Portal</span></a>
-                </li>
-                <li className="o-menu__item">
-                  <a className="o-menu__link" href="https://www.linode.com/lp/affiliate-program/"><span className="o-menu__title">Affiliate Program</span></a>
                 </li>
                 <li className="o-menu__item">
                   <a className="o-menu__link" href="https://www.linode.com/green-light/"><span className="o-menu__title">Beta Program</span></a>
